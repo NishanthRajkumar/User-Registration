275e7e3dd92a85964ee76022d8a4329906d975b0
Nishanth <rajkumarnishanth@gmail.com>
2022-02-28 17:28:20 +0530

    removed an unnecessary parameter from datarows and testmethod

--- RegistrationUnitTesting/EmailValidityTests.cs
+++ RegistrationUnitTesting/EmailValidityTests.cs
@@ -6,22 +6,22 @@ namespace RegistrationUnitTesting
     [TestClass]
     public class EmailValidityTests
     {
-        const string emailPattern = @"^[A-Za-z0-9]{3,}([\.\-\+][A-Za-z0-9]{3,})?[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2,})?$";
+        const string emailPattern = @"^[A-Za-z0-9]{3,}([\.\-\+][A-Za-z0-9]{3,})?[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2,3})?$";
 
         /// <summary>
         /// Tests the valid emails
         /// </summary>
         [TestMethod]
-        [DataRow(@"abc@yahoo.com", emailPattern)]
-        [DataRow(@"abc-100@yahoo.com", emailPattern)]
-        [DataRow(@"abc.100@yahoo.com", emailPattern)]
-        [DataRow(@"abc111@abc.com", emailPattern)]
-        [DataRow(@"abc-100@abc.net", emailPattern)]
-        [DataRow(@"abc.100@abc.com.au", emailPattern)]
-        [DataRow(@"abc@1.com", emailPattern)]
-        [DataRow(@"abc@gmail.com.com", emailPattern)]
-        [DataRow(@"abc+100@gmail.com", emailPattern)]
-        public void TestValidEmails(string email, string pattern)
+        [DataRow(@"abc@yahoo.com")]
+        [DataRow(@"abc-100@yahoo.com")]
+        [DataRow(@"abc.100@yahoo.com")]
+        [DataRow(@"abc111@abc.com")]
+        [DataRow(@"abc-100@abc.net")]
+        [DataRow(@"abc.100@abc.com.au")]
+        [DataRow(@"abc@1.com")]
+        [DataRow(@"abc@gmail.com.com")]
+        [DataRow(@"abc+100@gmail.com")]
+        public void TestValidEmails(string email)
         {
             var result = Registration.IsValid(email, emailPattern);
             Assert.IsTrue(result);
@@ -31,20 +31,20 @@ namespace RegistrationUnitTesting
         /// Tests the invalid emails
         /// </summary>
         [TestMethod]
-        [DataRow(@"abc", emailPattern)]
-        [DataRow(@"abc@.com.my", emailPattern)]
-        [DataRow(@"abc123@gmail.a", emailPattern)]
-        [DataRow(@"abc123@.com", emailPattern)]
-        [DataRow(@"abc123@.com.com ", emailPattern)]
-        [DataRow(@".abc@abc.com", emailPattern)]
-        [DataRow(@"abc()*@gmail.com", emailPattern)]
-        [DataRow(@"abc@%*.com", emailPattern)]
-        [DataRow(@"abc..2002@gmail.com", emailPattern)]
-        [DataRow(@"abc.@gmail.com", emailPattern)]
-        [DataRow(@"abc@abc@gmail.com", emailPattern)]
-        [DataRow(@"abc@gmail.com.1a", emailPattern)]
-        [DataRow(@"abc@gmail.com.aa.au", emailPattern)]
-        public void TestInvalidEmails(string email, string pattern)
+        [DataRow(@"abc")]
+        [DataRow(@"abc@.com.my")]
+        [DataRow(@"abc123@gmail.a")]
+        [DataRow(@"abc123@.com")]
+        [DataRow(@"abc123@.com.com ")]
+        [DataRow(@".abc@abc.com")]
+        [DataRow(@"abc()*@gmail.com")]
+        [DataRow(@"abc@%*.com")]
+        [DataRow(@"abc..2002@gmail.com")]
+        [DataRow(@"abc.@gmail.com")]
+        [DataRow(@"abc@abc@gmail.com")]
+        [DataRow(@"abc@gmail.com.1a")]
+        [DataRow(@"abc@gmail.com.aa.au")]
+        public void TestInvalidEmails(string email)
         {
             var result = Registration.IsValid(email, emailPattern);
             Assert.IsFalse(result);
